8ab6ddaa0c4a6c706656861ce700d3b91f9a8ee5
Anurag Basu <BasuAnurag@SEASIAINFOTECH.COM>
2024-03-20 15:43:20 +0530

    Add icon to Logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaHome, FaInfoCircle, FaBook, FaPhone, FaChartBar, FaUser,FaSignInAlt  } from "react-icons/fa";
+import { FaHome, FaInfoCircle, FaBook, FaPhone, FaChartBar, FaUser,FaSignInAlt ,FaSignOutAlt  } from "react-icons/fa";
 import mindset from "../assets/mindset.jpeg";
 
 function Navbar({ username, userEmail }) {
@@ -113,7 +113,7 @@ function Navbar({ username, userEmail }) {
                   onClick={handleLogout}
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-serif"
                 >
-                  Logout
+                 <FaSignOutAlt className="inline-block mr-1" />  Logout
                 </button>
               </li>
             ) : null}
