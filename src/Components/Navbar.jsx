fa13ce8829f649bb40d603f9cbe2918c73a102d2
Anurag Basu <BasuAnurag@SEASIAINFOTECH.COM>
2024-03-27 15:04:25 +0530

    Make mindset logo a button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaHome, FaInfoCircle, FaBook, FaPhone, FaChartBar, FaUser,FaSignInAlt ,FaSignOutAlt  } from "react-icons/fa";
+import { FaHome, FaInfoCircle, FaBook, FaPhone, FaChartBar, FaUser, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import mindset from "../assets/mindset.jpeg";
 
 function Navbar({ username, userEmail }) {
@@ -17,10 +17,12 @@ function Navbar({ username, userEmail }) {
     <nav className="bg-white border-[#3D3B3C] dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <header className="flex justify-between items-center py-0.2px-1">
-          <img src={mindset} className="h-12" alt="Mindset Academy" />
-          <div className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-0 font-serif">
-            Mindset Academy
-          </div>
+          <Link to="/" className="cursor-pointer flex items-center"> {/* Added flex and items-center classes */}
+            <img src={mindset} className="h-12" alt="Mindset Academy" />
+            <div className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-2 font-serif"> {/* Added ml-2 for margin */}
+              Mindset Academy
+            </div>
+          </Link>
         </header>
 
         <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -113,7 +115,7 @@ function Navbar({ username, userEmail }) {
                   onClick={handleLogout}
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-serif"
                 >
-                 <FaSignOutAlt className="inline-block mr-1" />  Logout
+                  <FaSignOutAlt className="inline-block mr-1" />  Logout
                 </button>
               </li>
             ) : null}
